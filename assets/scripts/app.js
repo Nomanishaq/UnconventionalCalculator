e00f0bfeb0e41c620d46c91539385646fb310a44
Nomanishaq <nomanishaq251@gmail.com>
2020-02-15 18:08:43 +0500

    add console function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,40 +1,57 @@
 const defaultValue = 0;
 let changeValue = defaultValue;
+let logEntry = [];
 
-let userInput = ()=>{
+let userInput = () => {
     return parseInt(userValue.value);
 }
 
-let createAndWriteOutput =(operator,resultBeforCal,calNum)=>{
+let createAndWriteOutput = (operator, resultBeforCal, calNum) => {
     const calDescription = `${resultBeforCal} ${operator} ${calNum}`;
-    outputResult(changeValue,calDescription);
+    outputResult(changeValue, calDescription);
+}
+let writeLog = (operationIdentifier,prevResult,operationNumber,newReslt) =>{
+    let val = {
+        operationI: operationIdentifier,
+        prevRes: prevResult,
+        operationNum: operationNumber,
+        newRes: newReslt
+    }
+    logEntry.push(val);
+    console.log(val);
+    
 }
 
-let add = ()=>{
+let add = () => {
     let userValue = userInput();
     let initialVal = changeValue;
     changeValue = changeValue + userValue;
-    createAndWriteOutput('+',initialVal,userValue);
+    createAndWriteOutput('+', initialVal, userValue);
+    writeLog('add',initialVal,userValue,changeValue);
 }
-let sub = () =>{
+let sub = () => {
     let userValue = userInput();
     let initialVal = changeValue;
     changeValue = changeValue - userValue;
-    createAndWriteOutput('-',initialVal,userValue);
+    createAndWriteOutput('-', initialVal, userValue);
+    writeLog('Subtract',initialVal,userValue,changeValue);
+
 }
-let multi = () =>{
+let multi = () => {
     let userValue = userInput();
     let initialVal = changeValue;
     changeValue = changeValue * userValue;
-    createAndWriteOutput('*',initialVal,userValue);
+    createAndWriteOutput('*', initialVal, userValue);
+    writeLog('Multiply',initialVal,userValue,changeValue);
 }
-let divide = () =>{
+let divide = () => {
     let userValue = userInput();
     let initialVal = changeValue;
     changeValue = changeValue / userValue;
-    createAndWriteOutput('/',initialVal,userValue);
+    createAndWriteOutput('/', initialVal, userValue);
+    writeLog('Divide',initialVal,userValue,changeValue);
 }
-addBtn.addEventListener('click',add);
-divideBtn.addEventListener('click',divide);
-multiplyBtn.addEventListener('click',multi);
-subtractBtn.addEventListener('click',sub);
+addBtn.addEventListener('click', add);
+divideBtn.addEventListener('click', divide);
+multiplyBtn.addEventListener('click', multi);
+subtractBtn.addEventListener('click', sub);
